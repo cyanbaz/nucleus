f0ddb1f731e6ced65dffe46996e5c3a26be5a3c8
Caner Yanbaz <103248957+cyanbaz@users.noreply.github.com>
2026-03-29 19:56:40 +0200

    feat(gradle): build-logic gradle wrapper removed

--- src/integrationTest/kotlin/de/cyanbaz/nucleus/NucleusApplicationTests.kt
+++ src/integrationTest/kotlin/de/cyanbaz/nucleus/NucleusApplicationTests.kt
@@ -1,13 +1,10 @@
 package de.cyanbaz.nucleus
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class NucleusApplicationTests {
     @Test
-    fun contextLoads() {
-        assertThat("").isEqualTo("")
-    }
+    fun contextLoads() {}
 }
